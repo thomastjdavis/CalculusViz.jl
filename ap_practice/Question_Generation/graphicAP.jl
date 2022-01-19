0c9b4d3c61730a12bc13cd531910d4ff974d10be
TJ Davis <thomas.davis@mavs.uta.edu>
2022-01-19 10:39:40 +0800

    Update graphicAP.jl
    Added an encoding scheme for the randomly generated graph - will change to make it less obvious to students/whoever using the graph.

--- ap_practice/Question_Generation/graphicAP.jl
+++ ap_practice/Question_Generation/graphicAP.jl
@@ -1,6 +1,6 @@
 #module GraphicReasoning
 
-using Calculus
+using Calculus, Plots
 
 
 #puts a semiCircle on the graph starting at x=a,y=fa, and going until x=b.
@@ -32,7 +32,6 @@ end
 #integral is line(b)*(b-a)
 function line(a::Int,fa,b::Int)
 	y = range(-5,5,length=11)
-	cond = true
 	fb = makeLineEnd(fa)
 	
 	m = (fb-fa)/(b-a)
@@ -42,14 +41,50 @@ end
 
 
 
+#=need 2 things:
+	1. encoded code as json storing info of the question
+		to be interpreted and converted to question text
+		given a code, make a question, and 
+	2. a
+=#
 
-function plotQuestion()
+function stringIt(z)
+	if z<0
+		return "n$(abs(z))"
+	else
+		return "$z"
+	end
+end
+
+function describeGraph(s::String)
+	description = "The graph of f begins at (-5,0) and is"
+	xPos, sPos = -5, 5
+	ypos = 0
+	lPos = lastindex(s)
+	while(xPos<5 && sPos<lPos)
+		m=1
+		if s[sPos]=='L'
+			
+			if s[sPos+1]=='n'
+				m=-1
+			end
+			description *= "a line from ($(xPos),("	
+		else
+			
+		end
+	end
+	
+end
+
+function plotQuestion(pictureName="question.png")
 	p = plot()
 
 	d = [2,3,4]
 	y_start = 0
 	pos = -5
 	ys = []
+	codeString ="Sn50" #starts at (-5,0)
+	
 	while pos<=5
 		ds = rand(d)
 		is_line = rand([true,false])
@@ -59,12 +94,22 @@ function plotQuestion()
 				txt = "($(pos+ds),$(y_start))"
 				plot!(f,pos,pos+ds;annotation=(pos+ds,y_start,text(txt,8)))
 				push!(ys,y_start)
+				
+				codeString *= "L$(stringIt(pos+ds))$(stringIt(y_start))"
 			else
+				if ds==3
+					r="$(ds)d2"	
+				else
+					r ="$(Integer(ds/2))"
+				end
+
 				f = semiCircle(pos,y_start,pos+ds)
 				plot!(f,pos,pos+ds)
 				annotate!([(pos+ds,y_start,Plots.text("($(pos+ds),$(y_start))",:left,8))])
 				annotate!([(pos+(ds/2),f(pos+(ds/2)),Plots.text("($pos,$(f(pos+(ds/2))))",:bottom,8))])
 				push!(ys,f(pos+(ds/2)))
+				
+				codeString *= "C$(r)"
 			end
 			if pos+ds>5
 				push!(ys,pos+ds)
@@ -80,15 +125,14 @@ function plotQuestion()
 	plot!(
 	xlims=[y0,yf],
 	ylims=[y0,yf],
-	title="graph of f",
+	title="$(codeString)",
 	minorgrid=true,
 	size=(600,600),
 	legend=false
 	)
-	
-	savefig("question.png")
-	
-
+	savefig(pictureName)
 end
 
 #end
+
+
