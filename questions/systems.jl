a1d77f5c41bf405626114adf2b83bec91b3046d5
TJ Davis <thomas.davis@mavs.uta.edu>
2022-05-07 16:41:13 +0800

    Create systems.jl
    Creating random systems of equations for students to solve

---
+++ questions/systems.jl
@@ -0,0 +1,40 @@
+using SymbolicUtils
+SymbolicUtils.show_simplified[] = false
+@syms x::Real y::Real z::Real
+
+coeffs = -10:1:10 
+
+function system(n::Int)
+    
+    if (n>4) || (n<=1)
+        return ""
+    end
+    M = rand(coeffs,(n,n))
+    sols = rand(coeffs,n)
+    w = M*sols
+    println("""{"coefficients": $M,
+                "rh": $w,
+                "solutions": $sols}""" )
+     
+    if (n==2)
+        println("The system is :")
+        equations = M*[x,y]
+        index = 1;
+        for col in equations
+            println("$(col) = $(w[index])" )
+            index = index+1;
+        end
+        return ""
+    end
+    if (n==3)
+        println("The system is :")
+        equations = M*[x,y,z]
+        index = 1;
+        for col in equations
+            println("$(col) = $(w[index])" )
+            index = index+1;
+        end
+        return ""
+    end
+
+end
